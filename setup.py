0e3d52a6945ec5795a0c28be8286fffcd915d51a
shudeng <sdruc0611@163.com>
2024-07-15 23:08:13 +0800

    fix: version 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tong',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),  # 默认将找到当前目录下所有的包
     install_requires=[
         # 如果有依赖项，请在这里列出
